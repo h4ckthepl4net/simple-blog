86c3939047c70d7109a19cff679a61fe69fdff1b
Karapet Sahatchian <k.sahatchian@codeex.io>
2023-10-24 18:50:56 +0400

    Slight modification of initial configuration

--- backend/commands/MigrationManager.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace backend\commands;
-
-class MigrationManager {
-    public static function create($param) {
-        echo $param;
-    }
-}
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import reactDom from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./src/App"
 import {BrowserRouter} from "react-router-dom";
 
-const root = reactDom.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
