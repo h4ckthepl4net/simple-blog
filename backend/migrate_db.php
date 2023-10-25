0a550b2621e3b3f96c2f4a7459c44ba2b0b56ed3
Karapet Sahatchian <k.sahatchian@codeex.io>
2023-10-25 10:14:56 +0400

    Adds unique constraint to categories name

--- backend/migrate_db.php
+++ backend/migrate_db.php
@@ -19,7 +19,7 @@ $db->query("
 $db->query("
     CREATE TABLE IF NOT EXISTS categories (
         id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL
+        name VARCHAR(255) NOT NULL UNIQUE,
     );
 ");
 
