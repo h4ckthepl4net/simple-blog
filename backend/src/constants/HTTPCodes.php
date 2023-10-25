0b5f7e6912f9cb55fa3039e9ac51eeb1acc4b105
Karapet Sahatchian <k.sahatchian@codeex.io>
2023-10-25 15:33:33 +0400

    Adds HTTPCodes constants

---
+++ backend/src/constants/HTTPCodes.php
@@ -0,0 +1,11 @@
+<?php
+class HTTPCodes {
+    const OK = 200;
+    const CREATED = 201;
+    const BAD_REQUEST = 400;
+    const UNAUTHORIZED = 401;
+    const NOT_FOUND = 404;
+    const INTERNAL_SERVER_ERROR = 500;
+
+    const CONFLICT = 409;
+}
