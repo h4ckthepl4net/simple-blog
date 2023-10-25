232c9e9a047f7220da4f38b94999ed360108854a
Karapet Sahatchian <k.sahatchian@codeex.io>
2023-10-25 15:36:09 +0400

    Changes index.php to correctly handle application routing

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,1 +1,11 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+global $klein;
+$klein = new \Klein\Klein();
+
+require_once __DIR__ . '/routes/Auth.php';
+require_once __DIR__ . '/routes/Posts.php';
+
+$klein->dispatch();
+
