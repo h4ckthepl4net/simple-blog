353427f52c2990bc422730ebd3c68bafdb2f0ccf
Karapet Sahatchian <k.sahatchian@codeex.io>
2023-10-25 10:13:24 +0400

    Changes error handling for database_connection.php

--- backend/database_connection.php
+++ backend/database_connection.php
@@ -1,19 +1,15 @@
 <?php
 global $db;
 $db = null;
-try {
-    $host = getenv('MYSQL_HOST') ?: 'localhost';
-    $port = getenv('MYSQL_PORT') ?: 3306;
-    $dbname = getenv('MYSQL_DATABASE') ?: 'main';
-    $user = getenv('MYSQL_USER') ?: 'root';
-    $password = getenv('MYSQL_PASSWORD') ?: '';
 
-    $dbString = 'mysql:host='.$host.';port='.$port.';dbname='.$dbname;
+$host = getenv('MYSQL_HOST') ?: 'localhost';
+$port = getenv('MYSQL_PORT') ?: 3306;
+$dbname = getenv('MYSQL_DATABASE') ?: 'main';
+$user = getenv('MYSQL_USER') ?: 'root';
+$password = getenv('MYSQL_PASSWORD') ?: '';
 
-    echo $dbString;
+$dbString = 'mysql:host='.$host.';port='.$port.';dbname='.$dbname;
 
-    $db = new PDO($dbString, $user, $password);
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
+echo $dbString;
+
+$db = new PDO($dbString, $user, $password);
