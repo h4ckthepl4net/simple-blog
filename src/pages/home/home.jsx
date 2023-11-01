017597737505116abf00942aa701f4aa82fc7661
Karapet Sahatchian <k.sahatchian@codeex.io>
2023-11-01 23:03:18 +0400

    Adds pagination for posts page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,62 +1,98 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {PostsService} from "../../services/posts/PostsService";
-import {setInitialized, setInitializing, setPosts, setTotal} from "../../store/posts/posts";
+import {setLoading, setPosts, setTotal, setPage} from "../../store/posts/posts";
 import {PageHeader} from "../../components/page-header/page-header";
 import {PostPreview} from "../../components/post-preview/post-preview";
 import './home.scss';
+import ReactPaginate from "react-paginate";
 
 
 const Home = () => {
-    const isInitializingRef = useRef(false);
-    const isInitialized = useSelector(state => state.posts.initialized);
-    const isInitializing = useSelector(state => state.posts.initializing);
+    const isInitializing = useRef(false);
+    const isInitialized = useRef(false);
+    const [error, setError] = useState(null);
+    const isLoading = useSelector(state => state.posts.loading);
     const page = useSelector(state => state.posts.page);
     const limit = useSelector(state => state.posts.limit);
     const posts = useSelector(state => state.posts.posts);
+    const total = useSelector(state => state.posts.total);
     const dispatch = useDispatch();
 
+    const fetchPosts = async () => {
+        dispatch(setLoading(true));
+        const pageBefore = page;
+        try {
+            const result = await PostsService.getPosts(page, limit);
+            dispatch(setPosts(result.posts));
+            dispatch(setTotal(result.count));
+            setError(null);
+        } catch (e) {
+            if (page === pageBefore) {
+                dispatch(setPosts([]));
+                dispatch(setTotal(0));
+                setError({
+                    message: "An error occurred while fetching posts.",
+                    callback: fetchPosts,
+                    text: "Retry"
+                });
+            }
+        } finally {
+            if (page === pageBefore) {
+                dispatch(setLoading(false));
+            }
+        }
+    }
+
     useEffect(() => {
-        if (!isInitialized && !isInitializingRef.current) {
-            isInitializingRef.current = true;
-            dispatch(setInitializing(true));
+        if (!isInitializing.current) {
             (async () => {
-                const result = await PostsService.getPosts(page, limit);
-                dispatch(setPosts(result.posts));
-                dispatch(setTotal(result.total));
-                dispatch(setInitialized(true));
-                isInitializingRef.current = false;
-                dispatch(setInitializing(false));
+                isInitializing.current = true;
+                await fetchPosts();
+                isInitializing.current = false;
+                isInitialized.current = true;
             })();
         }
     }, []);
 
     useEffect(() => {
-
-    }, [page]);
+        if (isInitialized.current) {
+            (async () => {
+                await fetchPosts();
+            })();
+        }
+    }, [page, limit]);
 
     return (
         <div>
-            <PageHeader title="Posts" loading={isInitializing}/>
+            <PageHeader title="Posts" loading={isLoading} error={error}/>
             <div className="posts-container">
                 {
                     posts.map(post => (
-                        <PostPreview key={post.id} post={post}/>
+                        <PostPreview key={post.id} post={post} overlay={isLoading}/>
                     ))
                 }
-                <ul className="pagination">
-                    <li className="page-item">
-                        <a className="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li>
-
-                    <li className="page-item">
-                        <a className="page-link" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li>
-                </ul>
+                {   total > limit &&
+                    <ReactPaginate pageCount={total / limit}
+                                   forcePage={page - 1}
+                                   pageRangeDisplayed={1}
+                                   marginPagesDisplayed={1}
+                                   nextLabel={<span aria-hidden="true">&raquo;</span>}
+                                   previousLabel={<span aria-hidden="true">&laquo;</span>}
+                                   containerClassName={"pagination " + (isLoading ? "pagination-loading" : "")}
+                                   pageClassName="page-item"
+                                   pageLinkClassName="page-link"
+                                   activeClassName="active"
+                                   previousClassName="page-item"
+                                   previousLinkClassName="page-link"
+                                   nextClassName="page-item"
+                                   nextLinkClassName="page-link"
+                                   breakClassName="page-item"
+                                   breakLinkClassName="page-link"
+                                   disabledClassName="disabled"
+                                   onPageChange={(data) => dispatch(setPage(data.selected + 1))}
+                    />
+                }
             </div>
         </div>
     )
