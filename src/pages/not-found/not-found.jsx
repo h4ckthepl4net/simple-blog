06e2afb379f87152404946ed7c89828a645c258f
Karapet Sahatchian <k.sahatchian@codeex.io>
2023-11-01 21:36:02 +0400

    Adds Not Found page to show in case of no route has been found

---
+++ src/pages/not-found/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './not-found.scss';
+
+export const NotFound = () => {
+    return (
+        <div className="container">
+            <h1>404</h1>
+            <h5>Looks like you are lost</h5>
+            <h5>Page not found</h5>
+        </div>
+    );
+};
